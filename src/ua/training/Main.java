b7f866d0379c6e292783c2019996c5b6c3016067
Dima Shoolia <mrschulpov97@gmail.com>
2019-12-16 22:53:14 +0200

    1 commit
    add methods with stream
    Methods for:
    average Without Stream
    average With Stream
    min Element from massive
    min Element Index from massive
    count Zero Elements ( how many zero in massive )
    count Elements More Than Zero
    method for multiply all elements on 100

---
+++ src/ua/training/Main.java
@@ -0,0 +1,36 @@
+package ua.training;
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int [] array = new int[]{0,1,3,5,-1,7,9,2,4,6,8};
+
+    int summ = 0;
+    for (int i = 0; i < array.length; i++){
+        summ +=i;
+    }
+    double averageWithoutStream = (double) summ/array.length;
+
+        double averageWithStream = IntStream.of(array)
+                .average().orElse(0);
+
+        double minElement = IntStream.of(array).min().orElse(0);
+
+        int minElementIndex = IntStream.range(0,array.length)
+                .reduce((i,j) -> array[i] > array[j] ? j : i)
+                .orElse(0);
+
+        long countZeroElements = IntStream.of(array)
+                .filter(x -> x == 0).count();
+
+        long countElementsMoreThanZero = IntStream.of(array)
+                .filter(x->x>0).count();
+
+        IntStream.of(array)
+                .map(x -> x*100).forEach(System.out::println);
+
+    }
+}
